9c512d4f9e0b28b23da0e14e9ac9778425f94821
NickFitton <github@nfitton.com>
2020-07-28 19:10:28 +0100

    Initial commit with generated spring project

---
+++ src/test/java/com/nfitton/flowfly/FlowFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfitton.flowfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
